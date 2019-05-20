3c0db0bad4a52725e22f11fc30d1f84197920efb
mermaid-kato <mermaid.kato@gmail.com>
2019-05-20 13:59:24 +0000

    create clock

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'home#index'
   get '/about',   to: 'home#index'
   get '/contact', to: 'home#index'
+  get '/clock', to: 'home#index'
 
   namespace :api, format: 'json' do
     resources :tasks, only: [:index, :create, :update]
